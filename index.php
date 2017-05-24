aadf4afcfa4d1a3fdead9dffe0e5f4cfcdd46a01
Timothy Law <timlyg@yahoo.com>
2017-05-24 13:48:55 -0400

    update

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
   <title>ESL Schools in The United States</title>
 </head>
 <body>
-  <? echo "test";?>
-<table>
+  <? echo "Welcome, All schools listed provide I-20 for students";?>
+<table border="1">
 <tr>
-  <th>School</th><th>State</th>
+  <th>School</th><th>State</th><th>City</th><th>Student size</th><th>Staff Size</th><th>Language Support</th>
+  <td>New York General Consulting</td>
+  <td>New York</td>
+  <td>Chelsea</td>
+  <td>800</td>
+  <td>20></td>
+  <td>English, Korean, Chinese, Japanese, Spanish, Russian</td>
 </tr>
 </table>
 </body>
