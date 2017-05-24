409660f8aa8320d03b3b486269c2c7bf349331f4
Timothy Law <timlyg@yahoo.com>
2017-05-24 13:50:21 -0400

    update

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
 <table border="1">
 <tr>
   <th>School</th><th>State</th><th>City</th><th>Student size</th><th>Staff Size</th><th>Language Support</th>
+</tr>
+<tr>
   <td>New York General Consulting</td>
   <td>New York</td>
   <td>Chelsea</td>
   <td>800</td>
-  <td>20></td>
+  <td>20</td>
   <td>English, Korean, Chinese, Japanese, Spanish, Russian</td>
 </tr>
 </table>
